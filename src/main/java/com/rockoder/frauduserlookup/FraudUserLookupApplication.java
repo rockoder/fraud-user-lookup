effc2446ad1c2c3f93b47fd327265c5ce6c587f8
Ganesh Pagade <ganesh.pagade@tesco.com>
2018-08-25 20:53:04 +0530

    Initial project setup

---
+++ src/main/java/com/rockoder/frauduserlookup/FraudUserLookupApplication.java
@@ -0,0 +1,12 @@
+package com.rockoder.frauduserlookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudUserLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudUserLookupApplication.class, args);
+	}
+}
